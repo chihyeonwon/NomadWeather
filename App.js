cda390dbf31ff846055f9c794810b77178419b35
chihyeonWON <heungeob1003@gamil.com>
2021-11-21 13:27:12 +0900

    날씨를 측정할 도시 이름 생성

--- App.js
+++ App.js
@@ -5,14 +5,20 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      
+      <View style={styles.city}>
+        <Text>Seoul</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 1,  
     backgroundColor: "tomato",
   },
+  city:{
+    flex: 1,
+    backgroundColor:"blue",
+  },  
 });
